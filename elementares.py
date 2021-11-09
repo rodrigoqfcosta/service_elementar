12add088827508ce21bca978ac3b26f0f52152b0
Gustavo Moura <gustavorobert92@gmail.com>
2021-11-09 01:58:46 -0300

    Correção de nomenclatura do argumento e do resultado

--- elementares.py
+++ elementares.py
@@ -8,33 +8,33 @@ CORS(app)
 
 @app.route('/soma')
 def soma():
-    int_input_1 = request.args.get('int_input_1', type=int)
-    int_input_2 = request.args.get('int_input_2', type=int)
-    int_output = {'soma': (int_input_1 + int_input_2)}
+    int_input_1 = request.args.get('arg1', type=int)
+    int_input_2 = request.args.get('arg2', type=int)
+    int_output = {'result': (int_input_1 + int_input_2)}
     return jsonify(int_output)
 
 
 @app.route('/subtracao')
 def subtraia():
-    int_input_1 = request.args.get('int_input_1', type=int)
-    int_input_2 = request.args.get('int_input_2', type=int)
-    int_output = {'subtracao': (int_input_1 - int_input_2)}
+    int_input_1 = request.args.get('arg1', type=int)
+    int_input_2 = request.args.get('arg2', type=int)
+    int_output = {'result': (int_input_1 - int_input_2)}
     return jsonify(int_output)
 
 
 @app.route('/multiplicacao')
 def multiplica():
-    int_input_1 = request.args.get('int_input_1', type=int)
-    int_input_2 = request.args.get('int_input_2', type=int)
-    int_output = {'multiplicacao': (int_input_1 * int_input_2)}
+    int_input_1 = request.args.get('arg1', type=int)
+    int_input_2 = request.args.get('arg2', type=int)
+    int_output = {'result': (int_input_1 * int_input_2)}
     return jsonify(int_output)
 
 
 @app.route('/divisao')
 def divide():
-    int_input_1 = request.args.get('int_input_1', type=int)
-    int_input_2 = request.args.get('int_input_2', type=int)
-    int_output = {'divisao': (int_input_1 / int_input_2)}
+    int_input_1 = request.args.get('arg1', type=int)
+    int_input_2 = request.args.get('arg2', type=int)
+    int_output = {'result': (int_input_1 / int_input_2)}
     return jsonify(int_output)
 
 
